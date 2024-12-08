c139da4b7d266b00319609075b5f963b36522ed7
John Arhin <j_arhin@yahoo.co.uk>
2024-12-08 13:38:33 +0000

    executive summary and other changes

--- shotspotter_dashboard.py
+++ shotspotter_dashboard.py
@@ -51,6 +51,14 @@ year_range_min = full_combinations_df["year"].min()
 year_range_max = full_combinations_df["year"].max()
 
 
+# set wide space as default
+# https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
+st.set_page_config(
+    page_title="Shotspotter Dashboard",
+    page_icon="📊",
+    layout="wide"
+)
+
 select_year_slider = st.sidebar.select_slider("Year range:", options=select_year_range, value=(year_range_max, year_range_min))
 
 min_slider, max_slider = list(select_year_slider)[0], list(select_year_slider)[1]
@@ -58,8 +66,78 @@ min_slider, max_slider = list(select_year_slider)[0], list(select_year_slider)[1
 
 selected_df_year = full_combinations_df[((full_combinations_df["year"] <= max_slider) & (full_combinations_df["year"] >= min_slider))]
 
-# create tabs
-tab_events, tab_shell_casing, tab_injuries, tab_arrests = st.tabs(["Events", "Shell Casings", "Injuries", "Arrests"])
+
+# column names for looping
+list_column_names = ["event_counts", "shell_casings", "injuries", "arrests"]
+
+# dictionary of variables
+dict_variables_summary = {}
+
+# rounding
+default_rounding = 3
+
+# create the loop
+for my_col in list_column_names:
+
+    # create dataframe for aggregation
+    temp_agg =  selected_df_year.groupby("shotspotter_alert").agg(
+        min_var = pd.NamedAgg(
+            column = my_col,
+            aggfunc="min"
+        ),
+        max_var = pd.NamedAgg(
+            column = my_col,
+            aggfunc="max"
+        ),
+        mean_var = pd.NamedAgg(
+            column = my_col,
+            aggfunc="mean"
+        ),
+        count_var = pd.NamedAgg(
+            column= my_col, 
+            aggfunc="count"
+        ),
+        sum_var = pd.NamedAgg(
+            column= my_col, 
+            aggfunc="sum"
+        )
+    ).reset_index()
+
+    # extract summary variables
+    dict_variables_summary["min_var_true" + "_" +  my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == True]["min_var"].values[0])
+    dict_variables_summary["min_var_false" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == False]["min_var"].values[0])
+    dict_variables_summary["max_var_true" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == True]["max_var"].values[0])
+    dict_variables_summary["max_var_false" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == False]["max_var"].values[0])
+    dict_variables_summary["mean_var_true" + "_" + my_col] = round(float(temp_agg[temp_agg["shotspotter_alert"] == True]["mean_var"].values[0]), default_rounding)
+    dict_variables_summary["mean_var_false" + "_" + my_col] = round(float(temp_agg[temp_agg["shotspotter_alert"] == False]["mean_var"].values[0]), default_rounding)
+    dict_variables_summary["count_var_true" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == True]["count_var"].values[0])
+    dict_variables_summary["count_var_false" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == False]["count_var"].values[0])
+    dict_variables_summary["sum_var_true" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == True]["sum_var"].values[0])
+    dict_variables_summary["sum_var_false" + "_" + my_col] = int(temp_agg[temp_agg["shotspotter_alert"] == False]["sum_var"].values[0])
+
+    # word summary for sums
+    dict_variables_summary["sum_increase_decrease" + "_" + my_col] = "increase" if dict_variables_summary["sum_var_true" + "_" + my_col] >= dict_variables_summary["sum_var_false" + "_" + my_col] else "decrease"
+    dict_variables_summary["sum_increase_decrease" + "_prefix_" + my_col] = "an" if dict_variables_summary["sum_increase_decrease" + "_" + my_col] == "increase" else "a"
+    dict_variables_summary["sum_percentage_diff_" + my_col] = abs(dict_variables_summary["sum_var_false" + "_" + my_col] - dict_variables_summary["sum_var_true" + "_" + my_col]) / dict_variables_summary["sum_var_false" + "_" + my_col] 
+
+    # summary for means
+    dict_variables_summary["mean_diff_" + my_col] = float(dict_variables_summary["mean_var_true" + "_" + my_col] -  dict_variables_summary["mean_var_false" + "_" + my_col])
+    dict_variables_summary["mean_percentage_diff_" + my_col] = float((dict_variables_summary["mean_var_true" + "_" + my_col] -  dict_variables_summary["mean_var_false" + "_" + my_col]) / dict_variables_summary["mean_var_false" + "_" + my_col])
+
+    # summary for counts
+    dict_variables_summary["count_years_" + my_col] = int(
+        max(
+            dict_variables_summary["count_var_true_" + my_col], dict_variables_summary["count_var_false_" + my_col], 
+        )
+    )
+
+# all counts
+dict_variables_summary["count_years"] = max([dict_variables_summary["count_years_" + my_col] for my_col in list_column_names])
+
+
+
+# create tabs ------
+tab_executive_summary, tab_events, tab_shell_casings, tab_injuries, tab_arrests = st.tabs(["Executive Summary", "Events", "Shell Casings", "Injuries", "Arrests"])
 
 
 df_filtered = df[((df["year"] <= max_slider) & (df["year"] >= min_slider))]
@@ -85,46 +163,110 @@ df_filtered = df_filtered.assign(
     events = 1
 )
 
+# Fix colour
+selected_df_year["colour"] = np.where(
+    selected_df_year["shotspotter_alert"],
+    "#ff0800",
+    "#00ff2a"
+)
+
+
+# summary
+# "Events", "Shell Casings", "Injuries", "Arrests"
+# dict_variables_summary["sum_var_true" + my_col]
+# list_column_names = ["event_counts", "shell_casings", "injuries", "arrests"]
+with tab_executive_summary:
+
+    st.header("Number of Years")
+    st.write(f"We consider {dict_variables_summary["count_years"]} years of selected data.")
+    st.subheader("Events")
+    st.write(f"We find that a total of {dict_variables_summary["sum_var_false_event_counts"]} non-Shotspotter events compared to a total of {dict_variables_summary["sum_var_true_event_counts"]} Shotspotter events, which is {dict_variables_summary["sum_increase_decrease_event_counts"]} {dict_variables_summary["sum_increase_decrease_prefix_event_counts"]} of {dict_variables_summary["sum_percentage_diff_event_counts"]:.2%} when we consider the non-Shotspotter events as the baseline.")
+    st.subheader("Shell Casings")
+    st.write(f"We find that a total of {dict_variables_summary["sum_var_false_shell_casings"]} shell casings during non-Shotspotter events compared to a total of {dict_variables_summary["sum_var_true_shell_casings"]} shell casings during Shotspotter events, which is {dict_variables_summary["sum_increase_decrease_shell_casings"]} {dict_variables_summary["sum_increase_decrease_prefix_shell_casings"]} of {dict_variables_summary["sum_percentage_diff_shell_casings"]:.2%} when we consider the non-Shotspotter shell casings as the baseline.")
+    st.subheader("Injuries")
+    st.write(f"We find that a total of {dict_variables_summary["sum_var_false_injuries"]} injuries during non-Shotspotter events compared to a total of {dict_variables_summary["sum_var_true_injuries"]} injuries during Shotspotter events, which is {dict_variables_summary["sum_increase_decrease_injuries"]} {dict_variables_summary["sum_increase_decrease_prefix_injuries"]} of {dict_variables_summary["sum_percentage_diff_injuries"]:.2%} when we consider the non-Shotspotter injuries as the baseline.")
+    st.subheader("Arrests")
+    st.write(f"We find that a total of {dict_variables_summary["sum_var_false_arrests"]} arrests during non-Shotspotter events compared to a total of {dict_variables_summary["sum_var_true_arrests"]} arrests during Shotspotter events, which is {dict_variables_summary["sum_increase_decrease_arrests"]} {dict_variables_summary["sum_increase_decrease_prefix_arrests"]} of {dict_variables_summary["sum_percentage_diff_arrests"]:.2%} when we consider the non-Shotspotter arrests as the baseline.")
+
 with tab_events:
+
+    # header
+    st.header("Shotspotter Alerts")
+    st.write(f"We consider the events with no alerts as the baseline.")
+
+    # columns
+    b1, b2, b3, b4, b5 = st.columns(5)
+    b1.metric("No. of Yr", f"{dict_variables_summary["count_years_event_counts"]}")
+    b2.metric("Alert: Avg. No. Events Per Yr.", f"{dict_variables_summary["mean_var_true_event_counts"]:.2f}")
+    b3.metric("No alert: Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_var_false_event_counts"]:.2f}")
+    b4.metric("Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_diff_event_counts"]:.2f}")
+    b5.metric("Percent Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_percentage_diff_event_counts"]:.2%}")
 
     col_timeseries, col_maps = st.columns(2)
 
     with col_timeseries:
         # tab
-        st.header("Events")
+        st.header("Events Visualisations")
+        st.write(":red[Red]: Shotspotter alert;")
+        st.write(":green[Green]: No Shotspotter alert.")
         # st.dataframe(selected_df_year[['year', 'shotspotter_alert', 'event_counts']].reset_index(drop=True))
         st.line_chart(
             selected_df_year,
             x = "year",
             y = "event_counts",
-            color="shotspotter_alert"
+            color="colour"
         )
 
     with col_maps:
-        st.write("Red for an associated Shotspotter alert and Green for no shotspotter alert")
         st.map(data=df_filtered, latitude="latitude", longitude="longitute", color="colour", size='events')
 
-with tab_shell_casing:
+with tab_shell_casings:
+
+    # header
+    st.header("Shotspotter Alerts")
+    st.write(f"We consider the events with no alerts as the baseline.")
+
+    # columns
+    b1, b2, b3, b4, b5 = st.columns(5)
+    b1.metric("No. of Yr", f"{dict_variables_summary["count_years_shell_casings"]}")
+    b2.metric("Alert: Avg. No. Events Per Yr.", f"{dict_variables_summary["mean_var_true_shell_casings"]:.2f}")
+    b3.metric("No alert: Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_var_false_shell_casings"]:.2f}")
+    b4.metric("Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_diff_shell_casings"]:.2f}")
+    b5.metric("Percent Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_percentage_diff_shell_casings"]:.2%}")
+
 
     col_timeseries, col_maps = st.columns(2)
 
-
     with col_timeseries:
         # tab
-        st.header("Shell Casing")
+        st.header("Shell Casings Visualisations")
+        st.write(":red[Red]: Shotspotter alert;")
+        st.write(":green[Green]: No Shotspotter alert.")
         # st.dataframe(selected_df_year[['year', 'shotspotter_alert', 'shell_casings']].reset_index(drop=True))
         st.line_chart(
             selected_df_year,
             x = "year",
             y = "shell_casings",
-            color="shotspotter_alert"
+            color="colour"
         )
 
     with col_maps:
-        st.write("Red for an associated Shotspotter alert and Green for no shotspotter alert")
         st.map(data=df_filtered, latitude="latitude", longitude="longitute", color="colour", size='shell_casings')
 
 with tab_injuries:
+
+    # header
+    st.header("Shotspotter Alerts")
+    st.write(f"We consider the events with no alerts as the baseline.")
+
+    # columns
+    b1, b2, b3, b4, b5 = st.columns(5)
+    b1.metric("No. of Yr", f"{dict_variables_summary["count_years_injuries"]}")
+    b2.metric("Alert: Avg. No. Events Per Yr.", f"{dict_variables_summary["mean_var_true_injuries"]:.2f}")
+    b3.metric("No alert: Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_var_false_injuries"]:.2f}")
+    b4.metric("Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_diff_injuries"]:.2f}")
+    b5.metric("Percent Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_percentage_diff_injuries"]:.2%}")
+
 
     col_timeseries, col_maps = st.columns(2)
 
@@ -132,36 +274,50 @@ with tab_injuries:
     with col_timeseries:
 
         # tab
-        st.header("Injuries")
+        st.header("Injuries Visualisations")
+        st.write(":red[Red]: Shotspotter alert;")
+        st.write(":green[Green]: No Shotspotter alert.")
         #st.dataframe(selected_df_year[['year', 'shotspotter_alert', 'injuries']].reset_index(drop=True))
         st.line_chart(
             selected_df_year,
             x = "year",
             y = "injuries",
-            color="shotspotter_alert"
+            color="colour"
         )
 
     with col_maps:
-        st.write("Red for an associated Shotspotter alert and Green for no shotspotter alert")
         st.map(data=df_filtered, latitude="latitude", longitude="longitute", color="colour", size='injuries')
 
 
 with tab_arrests:
+
+    # header
+    st.header("Shotspotter Alerts")
+    st.write(f"We consider the events with no alerts as the baseline.")
+
+    # columns
+    b1, b2, b3, b4, b5 = st.columns(5)
+    b1.metric("No. of Yr", f"{dict_variables_summary["count_years_arrests"]}")
+    b2.metric("Alert: Avg. No. Events Per Yr.", f"{dict_variables_summary["mean_var_true_arrests"]:.2f}")
+    b3.metric("No alert: Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_var_false_arrests"]:.2f}")
+    b4.metric("Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_diff_arrests"]:.2f}")
+    b5.metric("Percent Diff. in Avg. No. of Events Per Yr.", f"{dict_variables_summary["mean_percentage_diff_arrests"]:.2%}")
 
     col_timeseries, col_maps = st.columns(2)
 
 
     with col_timeseries:
         # tab
-        st.header("Arrests")
+        st.header("Arrests Visualisations")
+        st.write(":red[Red]: Shotspotter alert;")
+        st.write(":green[Green]: No Shotspotter alert.")
         # st.dataframe(selected_df_year[['year', 'shotspotter_alert', 'arrests']].reset_index(drop=True))
         st.line_chart(
             selected_df_year,
             x = "year",
             y = "arrests",
-            color="shotspotter_alert"
+            color="colour"
         )
 
     with col_maps:
-        st.write("Red for an associated Shotspotter alert and Green for no shotspotter alert")
         st.map(data=df_filtered, latitude="latitude", longitude="longitute", color="colour", size='arrests')
